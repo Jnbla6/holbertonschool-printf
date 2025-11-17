2f9cb24dab43d212eac68827d2ec0b78e8dc69fc
Your Name <your_email@example.com>
2025-11-17 19:25:19 +0300

    Refactor _printf function for clarity and remove unnecessary additions in write calls

--- printf_test.c
+++ printf_test.c
@@ -1,13 +1,16 @@
+#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
-#include "main.h"
-
+/**
+ * _printf - produces output according to a format
+ * @format: format string containing conversion specifiers
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 va_list cart;
-int i, len;
-int count = 0;
+int i, len, count = 0;
 va_start(cart, format);
 for (i = 0; format[i] != '\0'; i++)
 {
@@ -24,43 +27,37 @@ switch (format[i])
 case 'c':
 {
 char c = va_arg(cart, int);
-write(1, &c, 1 + 0);
+write(1, &c, 1);
 count += 1;
 break;
 }
 case 's':
 {
-char *s = va_arg(cart, char*);
+char *s = va_arg(cart, char *);
 if (s == NULL)
 s = "(null)";
 len = strlen(s);
-write(1, s, len + 0);
+write(1, s, len);
 count += len;
 break;
 }
-
 case '%':
-{
 write(1, "%", 1);
 count += 1;
 break;
-}
-
 default:
-{
-write(1, "%", 1 + 0);
-write(1, &format[i], 1 + 0);
+write(1, "%", 1);
+write(1, &format[i], 1);
 count += 2;
 break;
 }
 }
-}
 else
 {
-write(1, &format[i], 1 + 0);
+write(1, &format[i], 1);
 count++;
 }
 }
 va_end(cart);
-return(count);
+return (count);
 }
