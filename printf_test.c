4590253fc0e0a96047189ca021ce42abb8561732
Your Name <your_email@example.com>
2025-11-17 19:41:09 +0300

    Refactor _printf function documentation and improve code formatting for clarity

--- printf_test.c
+++ printf_test.c
@@ -3,9 +3,9 @@
 #include <unistd.h>
 #include <string.h>
 /**
- * _printf - produces output according to a format
+ * _printf - produces output
  * @format: format string
- * Return: number of characters printed
+ * Return: chars printed
  */
 int _printf(const char *format, ...)
 {
@@ -18,23 +18,35 @@ if (format[i] == '%')
 {
 i++;
 if (format[i] == '\0')
-{
-va_end(cart);
 return (-1);
-}
 switch (format[i])
 {
-case 'c':
-{ char c = va_arg(cart, int);
-write(1, &c, 1); count++; break; }
-case 's':
-{ char *s = va_arg(cart, char *);
-if (s == NULL) s = "(null)";
-len = strlen(s); write(1, s, len); count += len; break; }
+case 'c':;
+{
+char c = va_arg(cart, int);
+write(1, &c, 1);
+count++;
+break;
+}
+case 's':;
+{
+char *s = va_arg(cart, char *);
+if (s == NULL)
+s = "(null)";
+len = strlen(s);
+write(1, s, len);
+count += len;
+break;
+}
 case '%':
-write(1, "%", 1); count++; break;
+write(1, "%", 1);
+count++;
+break;
 default:
-write(1, "%", 1); write(1, &format[i], 1); count += 2; break;
+write(1, "%", 1);
+write(1, &format[i], 1);
+count += 2;
+break;
 }
 }
 else
