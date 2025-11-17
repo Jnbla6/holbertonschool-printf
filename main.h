0536a8f18878e41312d349287b887ff8e5aa49ec
csrama <ramadafer1@gmail.com>
2025-11-17 19:36:31 +0000

    merge and updated function

--- main.h
+++ main.h
@@ -17,4 +17,6 @@ int print_number(long n);
 int handle_specifier(const char *format, int *i, va_list cart);
 int print_char(char c);
 int print_string(char *s);
+int print_int(va_list args, char buffer[], int *buff_ind);
+int print_int_recursive(unsigned int num, char buffer[], int *buff_ind);
 #endif
