2548b0a235ce60e746c05a0e73df73684f5b02e9
csrama <ramadafer1@gmail.com>
2025-11-18 22:53:17 +0000

    core helper functions for _printf

--- functions.c
+++ functions.c
@@ -89,3 +89,112 @@ int print_int(va_list args)
 
     return count;
 }
+/**
+ * print_unsigned - prints unsigned integers
+ * @args: variadic list
+ * Return: number of characters printed
+ */
+int print_unsigned(va_list args)
+{
+    unsigned int n = va_arg(args, unsigned int);
+    return (print_unsigned_number(n));
+}
+
+/**
+ * print_octal - prints octal numbers
+ * @args: variadic list
+ * Return: number of characters printed
+ */
+int print_octal(va_list args)
+{
+    unsigned int n = va_arg(args, unsigned int);
+    return (print_octal_number(n));
+}
+
+/**
+ * print_hex - prints hexadecimal numbers
+ * @args: variadic list
+ * @uppercase: 1 for uppercase, 0 for lowercase
+ * Return: number of characters printed
+ */
+int print_hex(va_list args, int uppercase)
+{
+    unsigned int n = va_arg(args, unsigned int);
+    return (print_hex_number(n, uppercase));
+}
+
+/**
+ * print_unsigned_number - recursive function for unsigned numbers
+ * @n: unsigned number
+ * Return: count of characters
+ */
+int print_unsigned_number(unsigned int n)
+{
+    int count = 0;
+
+    if (n / 10)
+        count += print_unsigned_number(n / 10);
+
+    count += _putchar((n % 10) + '0');
+    return (count);
+}
+
+/**
+ * print_octal_number - converts to octal and prints
+ * @n: unsigned integer
+ * Return: number of characters printed
+ */
+int print_octal_number(unsigned int n)
+{
+    int count = 0;
+    char buffer[12];
+    int i = 0;
+
+    if (n == 0)
+        return _putchar('0');
+
+    while (n > 0)
+    {
+        buffer[i] = (n % 8) + '0';
+        n = n / 8;
+        i++;
+    }
+
+    while (i > 0)
+    {
+        count += _putchar(buffer[i - 1]);
+        i--;
+    }
+    return count;
+}
+
+/**
+ * print_hex_number - converts to hex and prints
+ * @n: unsigned integer
+ * @uppercase: 1 for uppercase, 0 for lowercase
+ * Return: number of characters printed
+ */
+int print_hex_number(unsigned int n, int uppercase)
+{
+    int count = 0;
+    char buffer[9];
+    int i = 0;
+    char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+
+    if (n == 0)
+        return _putchar('0');
+
+    while (n > 0)
+    {
+        buffer[i] = digits[n % 16];
+        n = n / 16;
+        i++;
+    }
+
+    while (i > 0)
+    {
+        count += _putchar(buffer[i - 1]);
+        i--;
+    }
+    return count;
+}
