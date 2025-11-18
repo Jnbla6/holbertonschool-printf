8b24c5b840aadd3fd03293de3b3f1406752172aa
csrama <ramadafer1@gmail.com>
2025-11-18 10:33:15 +0000

    implement flag handling for numeric conversions

--- functions.c
+++ functions.c
@@ -82,3 +82,50 @@ int print_int(va_list args)
     long n = va_arg(args, int);
     return (print_number(n));
 }
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i = 0, count = 0;
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(args, format);
+
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
+                count += print_char(args);
+            else if (format[i] == 's')
+                count += print_string(args);
+            else if (format[i] == '%')
+                count += print_percent();
+            else if (format[i] == 'd' || format[i] == 'i')
+                count += print_int(args);
+            else if (format[i] == 'b')  // ADD THIS FOR BINARY
+                count += print_binary(args);
+            else
+            {
+                count += _putchar('%');
+                count += _putchar(format[i]);
+            }
+        }
+        else
+        {
+            count += _putchar(format[i]);
+        }
+        i++;
+    }
+
+    va_end(args);
+    return (count);
+}
