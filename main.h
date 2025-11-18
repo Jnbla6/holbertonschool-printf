71939709993b5c1723ff915a81013d9215827134
csrama <ramadafer1@gmail.com>
2025-11-18 10:16:49 +0000

    Implement main _printf function with format parsing

--- main.h
+++ main.h
@@ -11,6 +11,7 @@ int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(void);
 int print_int(va_list args);
+int print_binary(va_list args);
 
 /* Helpers */
 int _putchar(char c);
