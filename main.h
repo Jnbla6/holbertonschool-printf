78cb98ce17e8e20c9146496f228c6bc58a5b6d99
csrama <ramadafer1@gmail.com>
2025-11-18 22:27:20 +0000

    main.h header for _printf project

--- main.h
+++ main.h
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include <string.h>
 
-#define BUFFER_SIZE 1024
 int _printf(const char *format, ...);
 
 
@@ -14,21 +13,7 @@ int print_string(char *s);
 int print_int(va_list args);
 int print_binary(unsigned int n);
 
-/* Conversion handlers */
-int handle_conversion(const char **format, va_list args);
-int handle_long(const char **format, va_list args);
-int handle_short(const char **format, va_list args);
-
-int print_char(va_list args);
-int print_percent(va_list args);
-int print_unsigned(va_list args, int base, int uppercase);
-
-
-
-
-
 /* Helpers */
 int _putchar(char c);
 int print_number(long n);
-int print_number_base(unsigned long n, int base, int uppercase);
 #endif 
