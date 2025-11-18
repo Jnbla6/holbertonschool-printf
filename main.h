87f398d9909809764ceb7ffe38fd5a6ca7ce84ce
csrama <ramadafer1@gmail.com>
2025-11-18 05:17:44 +0000

    updating functions

--- main.h
+++ main.h
@@ -1,19 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
-#include <string.h>
 
-/* Main printf function */
 int _printf(const char *format, ...);
 
-/* Helper functions */
-int _putchar(char c);
+/* Print functions */
 int print_char(va_list args);
 int print_string(va_list args);
+int print_percent(void);
 int print_int(va_list args);
-int print_percent(va_list args);
+
+/* Helpers */
+int _putchar(char c);
 int print_number(long n);
-int handle_specifier(const char *format, int *i, va_list cart);
-int print_int_recursive(unsigned int num, char buffer[], int *buff_ind);
+
 #endif
