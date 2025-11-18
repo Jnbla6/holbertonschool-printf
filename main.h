4d9d4b11956c64749fee0ca0fcd1774f9d8ac7c3
csrama <ramadafer1@gmail.com>
2025-11-18 22:08:24 +0000

    updating main 'test'

--- main.h
+++ main.h
@@ -13,8 +13,22 @@ int _printf(const char *format, ...);
 int print_string(char *s);
 int print_int(va_list args);
 int print_binary(unsigned int n);
+
+/* Conversion handlers */
+int handle_conversion(const char **format, va_list args);
+int handle_long(const char **format, va_list args);
+int handle_short(const char **format, va_list args);
+
+int print_char(va_list args);
+int print_percent(va_list args);
+int print_unsigned(va_list args, int base, int uppercase);
+
+
+
+
+
 /* Helpers */
 int _putchar(char c);
 int print_number(long n);
-
+int print_number_base(unsigned long n, int base, int uppercase);
 #endif 
