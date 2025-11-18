4f5fa728add5560644706971f0186a92b3d40212
csrama <ramadafer1@gmail.com>
2025-11-18 17:21:00 +0000

    Add string.h include for string manipulation functions

--- main.h
+++ main.h
@@ -10,7 +10,7 @@ int _printf(const char *format, ...);
 /* Print functions */
 int print_string(char *s);
 int print_int(va_list args);
-
+int print_binary(unsigned int n);
 /* Helpers */
 int _putchar(char c);
 int print_number(long n);
