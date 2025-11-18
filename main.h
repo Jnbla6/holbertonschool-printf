47ed4da3a37ed2acc93b39d466fb06d000b10c3a
csrama <ramadafer1@gmail.com>
2025-11-18 22:50:19 +0000

    main.h header for _printf project

--- main.h
+++ main.h
@@ -12,7 +12,9 @@ int _printf(const char *format, ...);
 int print_string(char *s);
 int print_int(va_list args);
 int print_binary(unsigned int n);
-
+int print_unsigned(va_list args);
+int print_octal(va_list args);
+int print_hex(va_list args, int uppercase);
 /* Helpers */
 int _putchar(char c);
 int print_number(long n);
