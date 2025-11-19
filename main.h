1a56d231268974a0e2d4c37392e0d9b451ea67d2
csrama <ramadafer1@gmail.com>
2025-11-19 13:33:04 +0000

    Add print_string_escaped declaration for %S specifier

--- main.h
+++ main.h
@@ -3,25 +3,14 @@
 
 #include <stdarg.h>
 #include <unistd.h>
-#include <string.h>
+#include <stdint.h>
 
-int _printf(const char *format, ...);
-int handle_specifier(const char *format, int *i, va_list cart);
-
-/* Print functions */
-int print_string(char *s);
-int print_string_escaped(va_list args);
-int print_int(va_list args);
-int print_binary(unsigned int n);
-int print_unsigned(va_list args);
-int print_octal(va_list args);
-int print_hex(va_list args, int uppercase);
-int print_unsigned_number(unsigned int n);
-int print_octal_number(unsigned int n);
-int print_hex_number(unsigned int n, int uppercase);
-
-/* Helpers */
 int _putchar(char c);
-int print_number(long n);
+int print_string(char *str);
+int print_number(int n);
+int print_binary(unsigned int n);
+int print_pointer(void *ptr);
+int print_custom_string(char *str);
+int _printf(const char *format, ...);
 
-#endif
+#endif /* MAIN_H */
