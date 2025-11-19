2dc8adf136871e77bb4423d33e53b0dc3c97fa45
csrama <ramadafer1@gmail.com>
2025-11-19 12:12:40 +0000

    Add the case in handle_specifier

--- _printf.c
+++ _printf.c
@@ -6,17 +6,35 @@ int handle_specifier(const char *format, int *i, va_list cart)
 {
     char c;
 
+
     (*i)++;
-    if (!format[*i]) return (-1);
-    if (format[*i] == 'c') { c = va_arg(cart, int); write(1, &c, 1); return (1); }
-    if (format[*i] == 's') return (print_string(va_arg(cart, char *)));
-    if (format[*i] == 'S') return (print_string_escaped(cart));  // ← ADD THIS LINE
-    if (format[*i] == '%') { write(1, "%", 1); return (1); }
-    if (format[*i] == 'b') return (print_binary(va_arg(cart, unsigned int)));
-    if (format[*i] == 'u') return (print_unsigned(cart));
-    if (format[*i] == 'o') return (print_octal(cart));
-    if (format[*i] == 'x') return (print_hex(cart, 0));
-    if (format[*i] == 'X') return (print_hex(cart, 1));
+    if (!format[*i])
+        return (-1);
+    if (format[*i] == 'c')
+    {
+        c = va_arg(cart, int);
+        write(1, &c, 1);
+        return (1);
+    }
+    if (format[*i] == 's')
+        return (print_string(va_arg(cart, char *)));
+    if (format[*i] == 'S')
+        return (print_string_escaped(cart));
+    if (format[*i] == '%')
+    {
+        write(1, "%", 1);
+        return (1);
+    }
+    if (format[*i] == 'b')
+        return (print_binary(va_arg(cart, unsigned int)));
+    if (format[*i] == 'u')
+        return (print_unsigned(cart));
+    if (format[*i] == 'o')
+        return (print_octal(cart));
+    if (format[*i] == 'x')
+        return (print_hex(cart, 0));
+    if (format[*i] == 'X')
+        return (print_hex(cart, 1));
 
     write(1, "%", 1);
     write(1, &format[*i], 1);
