fe235f44982b8c38c1e13bc2f1d0e94f1ff9a0ef
csrama <ramadafer1@gmail.com>
2025-11-19 13:57:45 +0000

    Implement print_string_escaped for %S custom specifier

--- functions.c
+++ functions.c
@@ -34,3 +34,36 @@ int print_custom_string(char *str)
 
 	return (count);
 }
+/**
+ * print_string_escaped - prints string with non-printable characters escaped
+ * @args: variadic list
+ * Return: number of characters printed
+ */
+int print_string_escaped(va_list args)
+{
+    char *s = va_arg(args, char *);
+    int count = 0, i;
+    char hex_digits[] = "0123456789ABCDEF";
+
+    if (!s)
+        s = "(null)";
+
+    for (i = 0; s[i]; i++)
+    {
+        /* Check if character is non-printable (0 < ASCII < 32 or >= 127) */
+        if ((s[i] > 0 && s[i] < 32) || s[i] >= 127)
+        {
+            count += write(1, "\\x", 2);
+            /* Print first hex digit (upper case) */
+            count += write(1, &hex_digits[(unsigned char)s[i] / 16], 1);
+            /* Print second hex digit (upper case) */
+            count += write(1, &hex_digits[(unsigned char)s[i] % 16], 1);
+        }
+        else
+        {
+            count += write(1, &s[i], 1);
+        }
+    }
+
+    return (count);
+}
