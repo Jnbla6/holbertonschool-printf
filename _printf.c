6646878a18d32b6144c1a9db88992d2e45c9ca8e
csrama <ramadafer1@gmail.com>
2025-11-19 11:43:30 +0000

    Add the case in handle_specifier

--- _printf.c
+++ _printf.c
@@ -2,36 +2,37 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
+
 /**
  * _printf - produces output according to a format
  * @format: format string
- * @i: pointer to current index in format string
- * @cart: va_list of arguments
  * Return: number of characters printed
  */
-int handle_specifier(const char *format, int *i, va_list cart);
-
 int _printf(const char *format, ...)
 {
-va_list cart;
-int i, count = 0;
+    va_list cart;
+    int i, count = 0;
 
-if (!format || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-va_start(cart, format);
-for (i = 0; format[i]; i++)
-{
-if (format[i] == '%')
-count += handle_specifier(format, &i, cart);
-else
-{
-write(1, &format[i], 1);
-count++;
+    if (!format || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
+    
+    va_start(cart, format);
+    
+    for (i = 0; format[i]; i++)
+    {
+        if (format[i] == '%')
+            count += handle_specifier(format, &i, cart);
+        else
+        {
+            write(1, &format[i], 1);
+            count++;
+        }
+    }
+
+    va_end(cart);
+    return (count);
 }
-}
-va_end(cart);
-return (count);
-}
+
 /**
  * handle_specifier - handles format specifiers
  * @format: format string
@@ -39,42 +40,49 @@ return (count);
  * @cart: va_list of arguments
  * Return: number of characters printed for the specifier
  */
-
 int handle_specifier(const char *format, int *i, va_list cart)
 {
-char c;
+    char c;
 
-(*i)++;
-if (!format[*i])
-return (-1);
-if (format[*i] == 'c')
-{
-c = va_arg(cart, int);
-write(1, &c, 1);
-return (1);
-}
-if (format[*i] == 's')
-return (print_string(va_arg(cart, char *)));
-if (format[*i] == '%')
-if (format[*i] == 'S')
-    return (print_string_escaped(cart, buffer, buf_index));
-{
-write(1, "%", 1);
-return (1);
-} 
-if (format[*i] == 'b') 
+    (*i)++;
+    if (!format[*i])
+        return (-1);
+    
+    if (format[*i] == 'c')
+    {
+        c = va_arg(cart, int);
+        write(1, &c, 1);
+        return (1);
+    }
+    
+    if (format[*i] == 's')
+        return (print_string(va_arg(cart, char *)));
+    
+    if (format[*i] == 'S')
+        return (print_string_escaped(cart));
+    
+    if (format[*i] == '%')
+    {
+        write(1, "%", 1);
+        return (1);
+    }
+    
+    if (format[*i] == 'b')
         return (print_binary(va_arg(cart, unsigned int)));
- if (format[*i] == 'u')
+    
+    if (format[*i] == 'u')
         return (print_unsigned(cart));
+    
     if (format[*i] == 'o')
         return (print_octal(cart));
+    
     if (format[*i] == 'x')
         return (print_hex(cart, 0));
+    
     if (format[*i] == 'X')
         return (print_hex(cart, 1));
 
-write(1, "%", 1);
-write(1, &format[*i], 1);
-return (2);
-} 
-
+    write(1, "%", 1);
+    write(1, &format[*i], 1);
+    return (2);
+}
