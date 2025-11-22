8c8b888a881bef08859b3d6ad15d1d2f33fc1d3b
csrama <ramadafer1@gmail.com>
2025-11-22 17:44:17 +0000

    0 flag character for non-custom conversion specifiers.

--- _printf.c
+++ _printf.c
@@ -59,18 +59,19 @@ int flags = 0;
 int length = LENGTH_NONE;
 int field_width = 0;
 int precision = -1;
-int zero_flag = 0;
+int zero_flag = 0;  /* zero flag */
 
 (*i)++;
 if (!format[*i])
 return (-1);
 
-while (format[*i] == '+' || format[*i] == ' ' || format[*i] == '#'|| format[*i] == '0' )
+/* ADD '0' TO FLAG DETECTION*/
+while (format[*i] == '+' || format[*i] == ' ' || format[*i] == '#' || format[*i] == '0')
 {
 if (format[*i] == '+') flags |= 1; /* flag for plus case*/
 if (format[*i] == ' ') flags |= 2; /* flag for space case*/
 if (format[*i] == '#') flags |= 4; /* flag for Hash case*/
- if (format[*i] == '0') zero_flag = 1; /* simple zero flag */
+if (format[*i] == '0') zero_flag = 1;  /* ADD THIS LINE for zero flag */
 (*i)++;
 if (!format[*i])
 break;
@@ -164,17 +165,17 @@ count_chars += _putchar(' ');
 return (count_chars);
 } 
  if (format[*i] == 'd' || format[*i] == 'i')
-        return (print_int(cart, flags, length, field_width, precision, zero_flag ));
+        return (print_int(cart, flags, length, field_width, precision, zero_flag));  /* ADD zero_flag*/
 if (format[*i] == 'b') 
         return (print_binary(va_arg(cart, unsigned int)));
  if (format[*i] == 'u')
-        return (print_unsigned(cart, flags, length, field_width, precision, zero_flag ));
+        return (print_unsigned(cart, flags, length, field_width, precision, zero_flag));  /* ADD zero_flag*/
     if (format[*i] == 'o')
-        return (print_octal(cart, flags ,length, field_width, precision,zero_flag ));
+        return (print_octal(cart, flags ,length, field_width, precision, zero_flag));  /*ADD zero_flag*/
     if (format[*i] == 'x')
-        return (print_hex(cart, 0, flags, length, field_width, precision,zero_flag ));
+        return (print_hex(cart, 0, flags, length, field_width, precision, zero_flag));  /* ADD zero_flag*/
     if (format[*i] == 'X')
-        return (print_hex(cart, 1, flags, length, field_width, precision,zero_flag ));
+        return (print_hex(cart, 1, flags, length, field_width, precision, zero_flag));  /* ADD zero_flag*/
     if (format[*i] == 'S')
     return (print_string_escaped(cart));
     if (format[*i] == 'p')
