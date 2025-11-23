5f0da9ec3057fcf65a281892ebb19ba2b1c29099
JNBLA6 <badrsha321@gmail.com>
2025-11-23 18:46:50 +0300

    addin commients to clarify the codes

--- _printf.c
+++ _printf.c
@@ -9,35 +9,35 @@
  * @cart: va_list of arguments
  * Return: number of characters printed
  */
-int handle_specifier(const char *format, int *i, va_list cart);
-void flush(void);
+int handle_specifier(const char *format, int *i, va_list cart); /* handle specifier function to handle wih each specify*/
+void flush(void); /*flush function to clean buffer*/
 
 int _printf(const char *format, ...)
 {
-va_list cart;
+va_list cart; /* our list named cart to handle vardic function*/
 int i, count = 0;
 
-if (!format || (format[0] == '%' && format[1] == '\0'))
-return (-1);
+if (!format || (format[0] == '%' && format[1] == '\0')) /*condition to handle if theres no specify*/
+    return (-1);
 
-if (!format || (format[0] == '%' && format[1] == ' ' && format[2] == '\0'))
-return (-1);
+if (!format || (format[0] == '%' && format[1] == ' ' && format[2] == '\0')) /*condition to handle if theres no specify*/
+    return (-1);
 
 
-va_start(cart, format);
+va_start(cart, format); /* start list*/
 
-for (i = 0; format[i]; i++)
+for (i = 0; format[i]; i++) /* loop to take each index*/
 {
-if (format[i] == '%')
-{
-count += handle_specifier(format, &i, cart);
-continue;
-}
+    if (format[i] == '%')
+    {
+    count += handle_specifier(format, &i, cart);
+    continue;
+    }
 
-if (i > 0 && format[i] == ' ' && format[i - 1] == '%')
-continue;
+    if (i > 0 && format[i] == ' ' && format[i - 1] == '%')
+    continue;
 
-count += _putchar(format[i]);
+    count += _putchar(format[i]);
 }
 flush();
 va_end(cart);
@@ -63,7 +63,7 @@ int left_justify = 0;  /* ADD THIS LINE */
 
 (*i)++;
 if (!format[*i])
-return (-1);
+    return (-1);
 
 /* ADD '-' TO FLAG DETECTION */
 while (format[*i] == '+' || format[*i] == ' ' || format[*i] == '#' || format[*i] == '-' || format[*i] == '0')
@@ -122,30 +122,30 @@ precision = precision * 10 + (format[*i] - '0');
 
 if (format[*i] == 'l' || format[*i] == 'h')
 {
-    length = (format[*i] == 'l') ? LENGTH_L : LENGTH_H;
-    (*i)++;
-    if (!format[*i])
-    {
-        count_chars += _putchar('%');
-        if (flags & 1) count_chars += _putchar('+');
-        if (flags & 2) count_chars += _putchar(' ');
-        if (flags & 4) count_chars += _putchar('#');
-        if (flags & 8) count_chars += _putchar('0');
-		if (left_justify) count_chars += _putchar('-');
-        return count_chars;
-    }
+length = (format[*i] == 'l') ? LENGTH_L : LENGTH_H;
+(*i)++;
+if (!format[*i])
+{
+count_chars += _putchar('%');
+if (flags & 1) count_chars += _putchar('+');
+if (flags & 2) count_chars += _putchar(' ');
+if (flags & 4) count_chars += _putchar('#');
+if (flags & 8) count_chars += _putchar('0');
+if (left_justify) count_chars += _putchar('-');
+return count_chars;
+}
 }
 
 if (!format[*i] && flags)  
 {
-    count_chars += _putchar('%');
-    if (flags & 1) count_chars += _putchar('+');
-    if (flags & 2) count_chars += _putchar(' ');
-    if (flags & 4) count_chars += _putchar('#');
-    if (flags & 8) count_chars += _putchar('0');
-	if (left_justify) count_chars += _putchar('-');
-    (*i)--;
-    return count_chars;
+count_chars += _putchar('%');
+if (flags & 1) count_chars += _putchar('+');
+if (flags & 2) count_chars += _putchar(' ');
+if (flags & 4) count_chars += _putchar('#');
+if (flags & 8) count_chars += _putchar('0');
+if (left_justify) count_chars += _putchar('-');
+(*i)--;
+return count_chars;
 }
 
 if ((flags & 1) && (flags & 2)) /* if plus flag and space flag combine*/
@@ -186,26 +186,26 @@ if (flags & 2)
 count_chars += _putchar(' ');
 return (count_chars);
 } 
- if (format[*i] == 'd' || format[*i] == 'i')
-        return (print_int(cart, flags, length, field_width, precision, left_justify));  /* ADD left_justify */
+if (format[*i] == 'd' || format[*i] == 'i')
+return (print_int(cart, flags, length, field_width, precision, left_justify));  /* ADD left_justify */
 if (format[*i] == 'b') 
-        return (print_binary(va_arg(cart, unsigned int)));
- if (format[*i] == 'u')
-        return (print_unsigned(cart, flags, length, field_width, precision, left_justify));  /* ADD left_justify */
-    if (format[*i] == 'o')
-        return (print_octal(cart, flags ,length, field_width, precision, left_justify));  /* ADD left_justify */
-    if (format[*i] == 'x')
-        return (print_hex(cart, 0, flags, length, field_width, precision, left_justify));  /* ADD left_justify */
-    if (format[*i] == 'X')
-        return (print_hex(cart, 1, flags, length, field_width, precision, left_justify));  /* ADD left_justify */
-    if (format[*i] == 'S')
-    return (print_string_escaped(cart));
-    if (format[*i] == 'p')
-    return (print_pointer(cart, field_width, left_justify));
-    if (format[*i] == 'r')
-    return (print_rev(cart));
-    if (format[*i] == 'R')
-    return (print_rot13(cart));
+return (print_binary(va_arg(cart, unsigned int)));
+if (format[*i] == 'u')
+return (print_unsigned(cart, flags, length, field_width, precision, left_justify));  /* ADD left_justify */
+if (format[*i] == 'o')
+return (print_octal(cart, flags ,length, field_width, precision, left_justify));  /* ADD left_justify */
+if (format[*i] == 'x')
+return (print_hex(cart, 0, flags, length, field_width, precision, left_justify));  /* ADD left_justify */
+if (format[*i] == 'X')
+return (print_hex(cart, 1, flags, length, field_width, precision, left_justify));  /* ADD left_justify */
+if (format[*i] == 'S')
+return (print_string_escaped(cart));
+if (format[*i] == 'p')
+return (print_pointer(cart, field_width, left_justify));
+if (format[*i] == 'r')
+return (print_rev(cart));
+if (format[*i] == 'R')
+return (print_rot13(cart));
 
 if (flags & 1)
 count_chars += _putchar('+');
